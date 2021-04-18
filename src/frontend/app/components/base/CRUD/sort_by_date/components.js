625d7468152780f2653042315f32733faef96611
Aliaksei <fiximgomel@gmail.com>
2021-04-19 00:31:16 +0300

    add sort date

--- src/frontend/app/components/base/CRUD/sort_by_date/components.js
+++ src/frontend/app/components/base/CRUD/sort_by_date/components.js
@@ -70,6 +70,9 @@ export function SortByDate(e) {
 
                     break;
                 };
+                case 'week': {
+                    
+                };
             }
         })
 }
